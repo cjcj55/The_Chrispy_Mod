01668e6a62e29e8b75b72a76b5a4f7f4390fa184
cjcj55 <47463790+cjcj55@users.noreply.github.com>
2022-02-01 16:55:16 -0500

    updated block loottables to 1.18, updated harvestLevels for blocks to 1.18 (tool.json, needs_tool.json, etc)

--- src/main/java/io/github/cjcj55/chrispymod/ChrispyMod.java
+++ src/main/java/io/github/cjcj55/chrispymod/ChrispyMod.java
@@ -48,8 +48,5 @@ public class ChrispyMod {
     }
 
     private void setup(final FMLCommonSetupEvent event) {
-        // some preinit code
-        LOGGER.info("HELLO FROM PREINIT");
-        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
     }
 }
