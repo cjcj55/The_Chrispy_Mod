588d3e8e4607345f9a9a350b94ed47cb80ffae23
cjcj55 <47463790+cjcj55@users.noreply.github.com>
2022-02-03 13:56:39 -0500

    fixed lightningSword

--- src/main/java/io/github/cjcj55/chrispymod/common/item/LightningSwordItem.java
+++ src/main/java/io/github/cjcj55/chrispymod/common/item/LightningSwordItem.java
@@ -2,10 +2,7 @@ package io.github.cjcj55.chrispymod.common.item;
 
 import io.github.cjcj55.chrispymod.common.itemgroups.ChrispyModItemGroups;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.LightningBolt;
-import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
@@ -17,16 +14,10 @@ public class LightningSwordItem extends SwordItem {
 
     @Override
     public boolean onLeftClickEntity(ItemStack stack, Player player, Entity entity) {
-        double x = entity.getX();
-        double y = entity.getY();
-        double z = entity.getZ();
-        Level level = player.getLevel();
-
-        LightningBolt lightningBolt = new LightningBolt(EntityType.LIGHTNING_BOLT, level);
-        lightningBolt.setPos(x, y, z);
-        ServerLevel serverLevel = (ServerLevel)level;
-        level.addFreshEntity(lightningBolt);
-
+        if(!player.level.isClientSide()) {
+            ServerLevel world = ((ServerLevel) player.level);
+            EntityType.LIGHTNING_BOLT.spawn(world, null, player, entity.blockPosition(), MobSpawnType.TRIGGERED, true, true);
+        }
         return false;
     }
 
