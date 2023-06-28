31c72d733b9baa02a19a5f62814a2769b570fad4
cjcj55 <47463790+cjcj55@users.noreply.github.com>
2023-06-28 14:15:38 -0400

    initial commit.  Basic shell of a mod running in 1.20

---
+++ src/main/java/github/cjcj55/chrispymod/ChrispyMod.java
@@ -0,0 +1,8 @@
+package github.cjcj55.chrispymod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("chrispymod")
+public class ChrispyMod {
+    public static final String MODID = "chrispymod";
+}
